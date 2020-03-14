705411f2a404a3f8eda90ccc6056d9290f9fc78a
nabil mek <nabilforfacebook@gmail.com>
2020-03-14 11:42:26 +0100

    imported the sys library

---
+++ 01_mynetmiko.py
@@ -0,0 +1,48 @@
+from netmiko import ConnectHandler
+import csv
+import re
+# router1={
+#         "device_type" : "cisco_ios",
+#         "ip" : "192.168.122.101",
+#         "username" : "nabil",    
+#         "password" : "cisco",
+#         "secret" : "secret"    
+
+# }
+# net_connect=ConnectHandler(**router1)
+# ouptput=net_connect.send_command("show ip int b")
+# print(ouptput)
+
+
+SW1={
+        "device_type" : "cisco_ios",
+        "ip" : "192.168.122.110",
+        "username" : "nabil",    
+        "password" : "cisco",
+        "secret" : "secret"    
+
+}
+
+SW2={
+        "device_type" : "cisco_ios",
+        "ip" : "192.168.122.111",
+        "username" : "nabil",    
+        "password" : "cisco",
+        "secret" : "secret"    
+
+}
+
+SWx=[SW1,SW2]
+
+for device in SWx:
+    print("***** This is SWITCH: "+ str(device["ip"]))
+    net_connect=ConnectHandler(**device)
+
+    for i in range(2,5):
+        config_commands=["vlan "+str(i), "name VLAN_"+str(i),  "exit"]
+        config=net_connect.send_config_set(config_commands)
+        print(config) # to see what's being configured in real time
+    ouptput=net_connect.send_command("show cdp neighbors")
+    print(ouptput)
+
+
